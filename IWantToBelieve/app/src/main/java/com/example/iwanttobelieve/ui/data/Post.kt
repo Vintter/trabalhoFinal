f4e1238b5a14518c40896d619948e546ca4b9382
Pattric Vintter <pattric.faria.pb@compasso.com.br>
2026-07-08 09:05:33 -0300

    feat: Create post

---
+++ IWantToBelieve/app/src/main/java/com/example/iwanttobelieve/ui/data/Post.kt
@@ -0,0 +1,14 @@
+package com.example.iwanttobelieve.ui.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "posts")
+data class Post(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val authorName: String,
+    val authorNickname: String,
+    val text: String,
+    val timestamp: String
+)
