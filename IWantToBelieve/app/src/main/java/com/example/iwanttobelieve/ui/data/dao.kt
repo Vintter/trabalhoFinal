4e92c73bc1a01f7ae0830b82a8c8e900a042ed2d
Pattric Vintter <pattric.faria.pb@compasso.com.br>
2026-07-08 09:14:52 -0300

    feat: Create dao

---
+++ IWantToBelieve/app/src/main/java/com/example/iwanttobelieve/ui/data/dao.kt
@@ -0,0 +1,27 @@
+package com.example.iwanttobelieve.ui.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PostDao {
+
+    @Query("SELECT * FROM posts ORDER BY id DESC")
+    fun getAllPosts(): Flow<List<Post>>
+
+    @Query("SELECT * FROM posts WHERE id = :id")
+    suspend fun getPostById(id: Int): Post?
+
+    @Insert
+    suspend fun insertPost(post: Post)
+
+    @Update
+    suspend fun updatePost(post: Post)
+
+    @Delete
+    suspend fun deletePost(post: Post)
+}
